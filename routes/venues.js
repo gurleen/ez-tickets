1bc594b81e4de2dbceda07fbbade9f212ed225ca
Gurleen Singh <gs585@drexel.edu>
2020-03-09 22:59:12 -0400

    Added GET venue by ID

--- routes/venues.js
+++ routes/venues.js
@@ -2,13 +2,33 @@ var express = require('express');
 var models = require('../models');
 var router = express.Router();
 
-/* GET all venues. */
+/* GET all venues */
 router.get('/', function(req, res, next) {
-    models.Venue.findAll().then(function(venues) {
+    models.Venue.findAll({
+        attributes: ['id', 'name', 'location', 'description']
+    }).then(function(venues) {
         res.setHeader('Content-Type', 'application/json');
         res.send(JSON.stringify(venues))
     })
 });
+
+/* GET venue by ID */
+router.get('/:id', function(req, res, next) {
+
+    models.Venue.findAll({
+        attributes: ['id', 'name', 'location', 'description'],
+        where: {
+            id: req.params['id'],
+        }
+    }).then(function(venue) {
+        if (venue.length === 0) {
+            res.status(500).send({ error: "No such venue exists." })
+            return
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(venue))
+    })
+})
 
 /* POST a new venue */
 router.post('/create', function(req, res) {
