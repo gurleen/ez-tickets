ae40a4425163a7c20892dd3e49a5f05985ebcefa
Gurleen Singh <gs585@drexel.edu>
2020-03-10 12:29:56 -0400

    Added User model and auth function

--- app.js
+++ app.js
@@ -15,6 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/auth', usersRouter);
 
 module.exports = app;
