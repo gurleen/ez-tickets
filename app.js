9f0186dffc60edf0a991a04c19bacf995be229cc
Gurleen Singh <gs585@drexel.edu>
2020-03-10 13:33:08 -0400

    Added JSON WebToken support

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express');
+var models = require('./models');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,11 +13,32 @@ var eventsRouter = require('./routes/events')
 
 var app = express();
 
+let ExtractJwt = passportJWT.ExtractJwt;
+let JwtStrategy = passportJWT.Strategy;
+let jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+jwtOptions.secretOrKey = 'changeThisInProduction';
+
+let strategy = new JwtStrategy(jwtOptions, async function(jwt_payload, next) {
+    console.log('payload received', jwt_payload);
+    let user = await models.User.findOne({
+        where: { username: jwt_payload.id },
+        attributes: ['username']
+    });
+    if (user) {
+        next(null, user);
+    } else {
+        next(null, false);
+    }
+});
+passport.use(strategy);
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', indexRouter);
 app.use('/auth', usersRouter);
